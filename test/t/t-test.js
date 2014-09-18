a9d7d5361624b4934a18d59af4ba8a36772829a9
Andrey Mokrousov <insanedeveloper@ya.ru>
2014-09-18 22:00:52 +0400

    context preservation & minimal templater

--- test/t/t-test.js
+++ test/t/t-test.js
@@ -1,5 +1,5 @@
 test('invocation', function(){
-   var generator = t.getGeneratorForNamespace('sample');
+    var generator = t.getGeneratorForNamespace('sample');
    /*-- minimal invocation abilities --*/
    /* generator is a function */
    generator('greeting', '<i>Hello</i>');
@@ -24,13 +24,36 @@ test('invocation', function(){
    equal(ns.greeting(), '<i>Hello</i>', 'template ok');
    
 });
+test('context', function(){
+    /* views must preserve context */
+    var generator = t.getGeneratorForNamespace('sample'),
+        view = t.getViewForNamespace('sample'),
+        context = {view:view};
+    generator('contextChecker', function(){
+        equal(this, context, 'Context preserved');
+        return Array.prototype.join.call(arguments, ', ');
+    });
+    view.call(context, 'contextChecker', {});
+    equal(context.view('contextChecker'), '', 'no args passed');
+    equal(context.view('contextChecker', 'ee'), 'ee', '1 arg passed');
+    equal(context.view('contextChecker', 'bla', 'alb'), 'bla, alb', '2 args passed');
+    equal(context.view('contextChecker', 'bla', 'alb', 42), 'bla, alb, 42', '3 args passed');
+    /* nested templates must preserve context */
+    generator('nestedChecker', '[% contextChecker %]');
+    equal(context.view('nestedChecker'), '', '[nested]no args passed');
+    equal(context.view('nestedChecker', 'ee'), 'ee', '[nested]1 arg passed');
+    equal(context.view('nestedChecker', 'bla', 'alb'), 'bla, alb', '[nested]2 args passed');
+    equal(context.view('nestedChecker', 'bla', 'alb', 42), 'bla, alb, 42', '[nested]3 args passed');
+    /* expect 17 test assertions */
+    expect(17);
+});
 test('template', function(){
     var generator = t.getGeneratorForNamespace('sample');
     var view = t.getViewForNamespace('sample')
     /* -- minimal template abilities -- */
     /* template syntax */
     generator('name', 'I am [% name %]');
-    equal(view('name', {name: 'Borg'}), 'I am Borg', 'simple substitution');
+    equal(view('name', {name: 'Borg'}), 'I am Borg', 'simple substitution && no recursion');
     equal(view('name', {designation: '1 of 9'}), 'I am ','zero data substitution');
     equal(view('name'), 'I am ','zero data invocation');
     generator('html', '<[% tag %][% attrs %]>[% content %]</[% tag %]>');
